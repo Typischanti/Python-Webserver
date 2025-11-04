6f60136811d51c3f9bdf635aa82a414d75563d9a
Anton Fröde <antonfroede@gmail.com>
2025-11-04 11:40:07 +0100

    Test#3

--- f4ask.py
+++ f4ask.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def Gesamtkilometer():
+    with open("Fahrtenbuch.txt", "r", encoding="utf-8") as f:
+        daten = f.read()
+    return render_template("index.html", daten=daten)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
